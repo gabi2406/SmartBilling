bc31249d4e536f6308c3942334e0536f48c6051d
valveluis <martinvalverde@gmail.com>
2025-09-12 11:05:40 -0600

    Fixing test case.

--- src/test/java/org/smartbilling/SmartbillingApplicationTests.java
+++ src/test/java/org/smartbilling/SmartbillingApplicationTests.java
@@ -1,9 +1,13 @@
 package org.smartbilling;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+//@SpringBootTest
+@DataJpaTest
+@ActiveProfiles("test")
 class SmartbillingApplicationTests {
 
 	@Test
